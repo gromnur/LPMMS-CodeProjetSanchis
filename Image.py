1bf2ee551088491ebf72f40ce4e65b09f3ce1493
Flavien <flavienregis@gmail.com>
2018-11-28 22:35:50 +0100

    Ajout de code déja realisé

---
+++ Image.py
@@ -0,0 +1,115 @@
+import exifread
+import os
+import sys
+import folium
+
+# Classe image contient les coordonnés GPS ainsi que la miniature de l'image si renseigné
+class Image(object):
+
+    # Renvoi : [%d,%d,%f] à partir d'un String : [%s, %s, %s]
+    def conversionTabNombre(GPSTab = "") :
+        var = GPSTab.replace("[","").replace("]","").split(", ")
+        seconde = list(map(int, var[2].split("/"))) #Conversion en Int
+        tab = [int(var[0]),int(var[1]),seconde[0]/seconde[1]]
+        return tab
+
+    def __init__(self, ImageDateTime = "", GPSLatitudeRef = "",
+        GPSLatitude = "", GPSLongitudeRef = "",
+        GPSLongitude = "", JPEGThumbnail = "",  Nom = "") :
+
+        # Initialisation variable
+        self.Nom = Nom
+        self.ImageDateTime = ImageDateTime
+        self.JPEGThumbnail = JPEGThumbnail
+        self.GPSLatitudeRef = ""
+        self.GPSLatitude = ""
+        self.GPSLongitudeRef = ""
+        self.GPSLongitude = ""
+
+    ## Setter ##
+
+    def _set_Nom(self,Nom):
+        self.Nom = Nom
+
+    def _set_ImageDateTime(self,ImageDateTime):
+        self.ImageDateTime = ImageDateTime
+
+    def _set_GPSLatitudeRef(self,GPSLatitudeRef):
+        self.GPSLatitudeRef = GPSLatitudeRef
+
+    def _set_GPSLatitudeDD(self, GPSLatitude):
+        self.GPSLatitude = Image.conversionTabNombre(GPSLatitude)
+
+    def _set_GPSLongitudeRef(self, GPSLongitudeRef):
+        self.GPSLongitudeRef = GPSLongitudeRef
+
+    def _set_GPSLongitudeDD(self, GPSLongitude):
+        self.GPSLongitude = Image.conversionTabNombre(GPSLongitude)
+
+    def _set_JPEGThumbnail(self, JPEGThumbnail):
+        self.JPEGThumbnail = JPEGThumbnail
+
+
+    ## Getter ##
+
+    def _get_Nom(self):
+        return self.Nom
+
+    def _get_ImageDateTime(self):
+        return self.ImageDateTime
+
+    def _get_GPSLatitudeRef(self):
+        return self.GPSLatitudeRef
+
+    def _get_GPSLatitudeDD(self):
+        return self.GPSLatitude
+
+    def _get_GPSLongitudeRef(self):
+        return self.GPSLongitudeRef
+
+    def _get_GPSLongitudeDD(self):
+        return self.GPSLongitude
+
+    def _get_JPEGThumbnail(self):
+        return self.JPEGThumbnail
+
+    # Longitude sous la forme DMS
+    def _get_GPSLongitudeDMS(self):
+        coord = self.GPSLongitude[0] + self.GPSLongitude[1]/60 + self.GPSLongitude[2]/3600
+        if self.GPSLongitudeRef == "E" :
+            return coord
+        if self.GPSLongitudeRef == "O" :
+            return -1 * coord
+        return None
+
+    # Latitude sous la forme DMS
+    def _get_GPSLatitudeDMS(self):
+        coord = self.GPSLatitude[0] + self.GPSLatitude[1]/60 + self.GPSLatitude[2]/3600
+        if self.GPSLatitudeRef == "N" :
+            return coord
+        if self.GPSLatitudeRef == "S" :
+            return -1 * coord
+        return None
+
+    # Verifie si l'image à des coordonné
+    def has_coord(self) :
+        return ((self.GPSLatitudeRef != "") &
+                (str(self.GPSLatitude) != "") &
+                (self.GPSLongitudeRef != "") &
+                (str(self.GPSLongitude) != ""))
+
+    # Comparateur permet a la méthode sort de fonctionné correctement
+    def __str__(self) :
+        return self.Nom + " " + self.ImageDateTime
+
+    def __repr__(self) :
+        return "\n" + self.Nom + " " + self.ImageDateTime + " lat:" + str(self.GPSLatitude) + " long:" + str(self.GPSLongitude)
+
+    def __eq__(self,other) :
+        return (self.ImageDateTime == other.ImageDateTime)
+
+    def __ne__(self,other) :
+        return (self.ImageDateTime != other.ImageDateTime)
+
+    def __lt__(self,other) :
+        return (self.ImageDateTime < other.ImageDateTime)
---
+++ testOuvertureJPG.py
@@ -0,0 +1,71 @@
+import exifread
+import os
+import sys
+import folium
+from Image import Image
+
+listImage = []
+
+# Chemin du dossier ou l'on recupere les images
+cheminDossier = 'D:\TDIMGFoluim\Image'
+dirImage = os.listdir(cheminDossier)
+
+# Ouverture d'un fichier log
+fsorti = open('testlog.txt','w')
+# Parcour du dossier d'images
+for index in range(0,len(dirImage)) :
+
+    # Table des données recupéré par photo
+    contenu = {}
+
+    # Ouverture d'une image
+    with open(cheminDossier + '\\' + dirImage[index],'rb') as image :
+        # Séparation dans le fichier
+        print ("\n\nImage " + str(index) + ":\n" ,file = fsorti)
+
+        #Ouverture du EXIF de l'image JPG
+        tags = exifread.process_file(image)
+
+        #Parcour des tag EXIF de l'image JPG ouverte précédament
+        for tag in tags.keys():
+            # On garde les tag souhaité
+            if tag in ('GPS GPSLatitudeRef',
+                       'GPS GPSLatitude',
+                       'GPS GPSLongitudeRef',
+                       'GPS GPSLongitude',
+                       'Image DateTime') :
+                print ("Key: %s, value %s" % (tag, tags[tag]), file=fsorti)
+                contenu[tag] = tags[tag].__str__()
+        #Créé une image avec son nom
+        anImage = Image(Nom = dirImage[index])
+
+        # Ajoute la miniature
+        if (contenu.__contains__('Image DateTime')) :
+            anImage._set_ImageDateTime(contenu['Image DateTime'])
+
+        # Met a jour l'image si les coordonné existe
+        if (contenu.__contains__('GPS GPSLatitudeRef')) :
+            anImage._set_GPSLatitudeRef(contenu['GPS GPSLatitudeRef'])
+        if (contenu.__contains__('GPS GPSLatitude')) :
+            anImage._set_GPSLatitudeDD(contenu['GPS GPSLatitude'])
+        if (contenu.__contains__('GPS GPSLongitudeRef')) :
+            anImage._set_GPSLongitudeRef(contenu['GPS GPSLongitudeRef'])
+        if (contenu.__contains__('GPS GPSLongitude')) :
+            anImage._set_GPSLongitudeDD(contenu['GPS GPSLongitude'])
+
+        listImage.append(anImage)
+
+# Tri les image par ordre chronologique
+listImage.sort()
+
+# Affiche quels image ont des coordonné
+for img in listImage :
+    print(img.has_coord())
+
+print(listImage[0].__repr__())
+
+a = [listImage[0]._get_GPSLatitudeDMS(),listImage[0]._get_GPSLongitudeDMS()]
+# Création de la carte html
+map_osm = folium.Map(location=a)
+folium.Marker(a).add_to(map_osm)
+map_osm.save('map.html')
