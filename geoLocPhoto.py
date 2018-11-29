47af3c8a1c1f631f0b2600033b2bf536bc87f545
gael <gael.parra.lurcat@gmail.com>
2018-11-29 17:58:38 +0100

    nouveau fichier

---
+++ geoLocPhoto.py
@@ -0,0 +1,13 @@
+import exifread
+import os
+import sys
+from ImageCoord import ImageCoord
+
+if (len(sys.argv) != 2):
+	print("syntaxe : python geoLocPhoto.py image")
+	exit(1)
+
+image = "Image/"+ImageCoord(sys.arv[1])
+
+image.__repr__()
+
