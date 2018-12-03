dec20424265ba2bd46a218b8ce087469c2b41969
Flavien <flavienregis@gmail.com>
2018-12-03 19:06:18 +0100

    Ajout listLocPhoto.py

--- geoLocPhoto.py
+++ geoLocPhoto.py
@@ -5,7 +5,7 @@ from ImageCoord import ImageCoord
 
 if (len(sys.argv) != 2):
 	print("syntaxe : python geoLocPhoto.py image")
-	exit(1)
+	
 
 image = ImageCoord(sys.argv[1])
 print(image.__repr__())
