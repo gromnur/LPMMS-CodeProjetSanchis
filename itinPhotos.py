349bae406f615ba7db101d35e36904900c352348
Flavien <flavienregis@gmail.com>
2018-12-06 17:53:03 +0100

    merge

--- itinPhotos.py
+++ itinPhotos.py
@@ -5,12 +5,8 @@ import sys
 import webbrowser
 
 # Chemin du dossier ou l'on recupere les images
-<<<<<<< HEAD
-cheminDossier = 'D:\\LPMMS\\ProjetSanchisIMG\\LPMMS-CodeProjetSanchis\Image'
-=======
 # cheminDossier = 'D:\\projet python\\LPMMS-CodeProjetSanchis\\Image'
 cheminDossier = os.path.abspath(sys.argv[1])
->>>>>>> 0eefc86d41049650bb7e4d65672ccc1f8fbe99d2
 dirImage = os.listdir(cheminDossier)
 
 #création du fichier tampon
